15c7f6253a03ddc47a22fd849abf9e094ce9e9cb
madmalware <mari.zombie2020@gmail.com>
2024-04-10 00:52:14 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('cadastro', [App\Http\Controllers\HomeController::class, 'index'])->name('cadastro');
