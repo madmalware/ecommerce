a2c046d645cf35022879d720242d3161013f5f78
madmalware <mari.zombie2020@gmail.com>
2024-05-04 20:44:05 -0300

    erro migração

---
+++ app/Models/Produto.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'categoria_id');
+    }
+
+    public function servico()
+    {
+        return $this->belongsTo(Servico::class, 'servico_id');
+    }
+
+}
